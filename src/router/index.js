2eeabdc7601ba72334e1ece488677eff6a093c76
Sreyno <sreynosoeury@gmail.com>
2025-11-07 16:26:00 +0700

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: {
+        template: '<h1>This is my first VueJs project.</h1>'
+      },
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component : {
+        template: '<h1>Welcome to home page</h1>'
+      },
+    }
+  ],
+})
+
+export default router
